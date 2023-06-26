b856326bfded6d77b655a793a2fc65be8f9769eb
Perz <s.ali224@mybvc.ca>
2023-06-25 18:22:56 -0600

    Upgrades to EnemyAI
    some fixes and upgraded the enemyai collision and intraction upon detection.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,24 +5,20 @@ using System.Collections;
 
 public class EnemyAI : MonoBehaviour
 {
-    // AI settings
     [Header("AI Settings")]
     [SerializeField] public Transform target;
     [SerializeField] public float chaseRange = 10f;
     [SerializeField] public float chargeSpeed = 15f;
     [SerializeField] public float chargeCooldown = 5f;
 
-    // Patrol points
     [Header("Patrol")]
     [SerializeField] public Transform pointA;
     [SerializeField] public Transform pointB;
     [SerializeField] public float patrolSpeed = 3f;
 
-    // Particle system
     [Header("Particle")]
     [SerializeField] public ParticleSystem chargeParticles;
 
-    // Exclamation mark for detection
     [Header("Exclamation")]
     [SerializeField] public Image exclamationMark;
     [SerializeField] public float detectionRange = 10f;
@@ -40,11 +36,9 @@ public class EnemyAI : MonoBehaviour
         agent.speed = patrolSpeed;
         currentPatrolPoint = pointA;
 
-        // Initialize Rigidbody and freeze rotation
         rb = GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeRotation;
         
-        // Make the particle system brighter
         var chargeParticleMain = chargeParticles.main;
         chargeParticleMain.startColor = new Color(1, 1, 1, 1);
     }
@@ -54,8 +48,6 @@ public class EnemyAI : MonoBehaviour
         if (target == null) return;
 
         float distanceToTarget = Vector3.Distance(target.position, transform.position);
-
-        // Show or hide exclamation mark based on distance
         exclamationMark.enabled = (distanceToTarget <= detectionRange);
 
         if (!isCharging)
@@ -76,55 +68,40 @@ public class EnemyAI : MonoBehaviour
         else
         {
             chargeTime -= Time.deltaTime;
+            // Move the enemy during the charge
+            rb.velocity = chargeDirection * chargeSpeed;
 
             if (chargeTime <= 0)
             {
                 isCharging = false;
                 agent.enabled = true;
-
-                // Disable gravity when not charging
-                rb.useGravity = false;
+                rb.velocity = Vector3.zero;  // Reset velocity when not charging
             }
         }
     }
 
-    // Patrol between pointA and pointB
     private void Patrol()
     {
         agent.SetDestination(currentPatrolPoint.position);
 
         if (Vector3.Distance(transform.position, currentPatrolPoint.position) < 1f)
         {
-            if (currentPatrolPoint == pointA)
-            {
-                currentPatrolPoint = pointB;
-            }
-            else
-            {
-                currentPatrolPoint = pointA;
-            }
+            currentPatrolPoint = (currentPatrolPoint == pointA) ? pointB : pointA;
         }
     }
 
-    // Coroutine for charging behavior
     private IEnumerator ChargeUp()
     {
         chargeParticles.Play();
         yield return new WaitForSeconds(2f);
         chargeParticles.Stop();
 
-        // Turn towards player
         transform.LookAt(target.position);
-        
-        // Pause for a second before charging
         yield return new WaitForSeconds(1f);
 
         isCharging = true;
         agent.enabled = false;
         chargeDirection = (target.position - transform.position).normalized;
         chargeTime = chargeCooldown;
-
-        // Enable gravity when charging
-        rb.useGravity = true;
     }
 }
