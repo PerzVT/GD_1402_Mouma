1192508aee5b37712b3c4f2f853c07721183fd4c
yufen <pengyufenft@outlook.com>
2023-08-09 00:55:28 -0600

    Add Start Scene

---
+++ Assets/Scripts/UI/StartMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject startMenuUI;
+
+    private void Start()
+    {
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene"); 
+    }
+
+    public void QuitGame()
+    {
+        // 退出游戏
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
