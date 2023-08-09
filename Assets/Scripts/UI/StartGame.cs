43ad57c265108f611945c070d4d4d0774b87da67
yufen <pengyufenft@outlook.com>
2023-08-09 13:06:41 -0600

    Change UI and add fur model

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -7,6 +7,6 @@ public class StartGame : MonoBehaviour
 {
     public void StartMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
